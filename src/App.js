ae8da2eeecdaa1479a806a88aa903585174d100b
Benjamin Legrand <jibhaine@free.fr>
2017-05-24 08:02:24 +0200

    initial commi

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 import React, { Component } from "react";
 import "./App.css";
 import PlayerNameInput from "./components/PlayerNameInput";
@@ -7,6 +10,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
         <PlayerNameInput />
         <Board
           cols={["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]}
